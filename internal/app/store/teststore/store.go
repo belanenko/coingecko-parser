b31dd3103e98212b8941bb892736aa52dc0e5c13
Tim <belanenkotimofey@gmail.com>
2022-05-04 08:42:25 +0300

    change returning values

--- internal/app/store/teststore/store.go
+++ internal/app/store/teststore/store.go
@@ -1,13 +1,16 @@
 package teststore
 
-import "github.com/belanenko/coingecko-parser/internal/app/model"
+import (
+	"github.com/belanenko/coingecko-parser/internal/app/model"
+	"github.com/belanenko/coingecko-parser/internal/app/store"
+)
 
 type Store struct {
 	db                map[string][]model.History
 	historyRepository *HistoryRepository
 }
 
-func New() *Store {
+func New() store.Store {
 	return &Store{
 		db: make(map[string][]model.History),
 	}
@@ -15,7 +18,7 @@ func New() *Store {
 
 // Игнорирую использование контекста/потом допилю
 
-func (s *Store) History() *HistoryRepository {
+func (s *Store) History() store.HistoryRepository {
 	if s.historyRepository != nil {
 		return s.historyRepository
 	}
